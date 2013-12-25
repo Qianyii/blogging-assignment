278e20c4e422b646e3af53333c928b9f7f5ac40b
Jaelyyn <tan.qianyi12@gmail.com>
2013-12-25 23:28:49 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,5 @@
+<form action="" method="post">
+<p><label>Username</label><input type="text" name="username" value=""  /></p>
+<p><label>Password</label><input type="password" name="password" value=""  /></p>
+<p><label></label><input type="submit" name="submit" value="Login"  /></p>
+</form>
